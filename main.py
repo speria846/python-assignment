1a26dd69f8a4992d20af4f5cc596019d17438984
speria846 <niwenyesigasperia@gmail.com>
2022-05-20 21:00:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def sum_and_greats(*number,**students):
+        sam=1
+        for x in number:
+         sam*=x
+         print(sam)
+        print(f"hello{students}") 
+
+          
+
+        
+                  
+                                  
+        
+            
+            
+         
